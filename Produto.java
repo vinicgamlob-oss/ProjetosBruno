1a318422b6636c182d0c622a60604537a47fd763
Vinicius Gama Bittencourt Lobo <vinicgamlob@gmail.com>
2026-03-27 20:38:24 -0300

    UploadDeArquivos

---
+++ Produto.java
@@ -0,0 +1,23 @@
+public class Produto {
+
+	private int Codigo;
+
+	private Fornecedor .Fornecedor Fornecedor;
+
+	private int Codigo_Barra;
+
+	private Lote lote;
+
+	public void CosultarProduto() {
+
+	}
+
+	public Produto CadastrarProduto(int Codigo, Fornecedor .Fornecedor Fornecedor, int CodigoDeBarra) {
+		return null;
+	}
+
+	public Produto AtulizarProduto(int Codigo, Fornecedor .Fornecedor Fornecedor, int CodigoDeBarra) {
+		return null;
+	}
+
+}
