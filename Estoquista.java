0e7b362be5da947f12fb025e6cea3abe7e941b02
Vinicius Gama Bittencourt Lobo <vinicgamlob@gmail.com>
2026-03-27 21:42:05 -0300

    Update

--- Estoquista.java
+++
@@ -1,9 +0,0 @@
-public class Estoquista extends Funcionario {
-
-	private Funcionario funcionario;
-
-	public void Registrar Entrada  do Lote() {
-
-	}
-
-}
--- Funcionario_e.java
+++
@@ -1,7 +0,0 @@
-public enum Funcionario_e {
-
-	Gerente,
-
-	Estoquista;
-
-}
